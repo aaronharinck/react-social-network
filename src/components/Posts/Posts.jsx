99e445e638f6efe645e8fef16fd2c285df44cc97
aaronharinck <56625177+aaronharinck@users.noreply.github.com>
2022-07-11 17:52:27 +0200

    refactor: cond. rend. posts comp. no longer req.

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -2,20 +2,11 @@ import React from "react";
 import { useObserver } from "mobx-react-lite";
 import PostsList from "../PostsList/PostsList";
 import PostForm from "../PostForm/PostForm";
-import { useStores } from "../../hooks/UseStores";
-import Login from "../../components/Login/Login";
 
 const Posts = () => {
-  const { uiStore } = useStores();
   return useObserver(() => (
     <>
-      {uiStore.currentUser === undefined ? (
-        <Login />
-      ) : (
-        <div className="addPost">
-          <PostForm />
-        </div>
-      )}
+      <PostForm />
       <PostsList />
     </>
   ));
